0b1c478c545cffd737a8369de1a0b12ac897fe4a
Garett Tok <garettok@hotmail.com>
2020-08-24 16:15:04 +0800

    Restored example

--- yaml_edit/example/example.dart
+++ yaml_edit/example/example.dart
@@ -2,19 +2,12 @@ import 'package:yaml/yaml.dart';
 import 'package:yaml_edit/yaml_edit.dart';
 
 void main() {
-//   final doc = YamlEditor('''
-// - 0 # comment 0
-// -
-// - 2 # comment 2
-// ''');
-//   doc.remove([1]);
-
-//   print(doc);
-
   final doc = YamlEditor('''
-key:
+- 0 # comment 0
+-
+- 2 # comment 2
 ''');
+  doc.remove([1]);
 
-  print((doc.parseAt([]) as YamlMap).nodes.keys.first.span);
-  print(doc.parseAt(['key']).span);
+  print(doc);
 }
