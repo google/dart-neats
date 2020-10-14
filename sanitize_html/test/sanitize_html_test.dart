ddaaa395fca1398a68431bc3e363c692ff0f8dbc
David Iglesias Teixeira <ditman@gmail.com>
2020-10-14 16:08:49 -0700

    dartfmt -w .

--- sanitize_html/test/sanitize_html_test.dart
+++ sanitize_html/test/sanitize_html_test.dart
@@ -12,44 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:meta/meta.dart' show required;
 import 'package:test/test.dart';
 import 'package:sanitize_html/sanitize_html.dart' show sanitizeHtml;
 
 void main() {
-
   // Calls sanitizeHtml with two different configurations.
-  //  * When `withOptionalParameters` is true, it calls the function with overrides
-  // for `allowElementId`, `allowClassName` and `addLinkRel`, for feature testing.
-  //  * When `withOptionalParameters` is false, it calls the function *only* with
-  // the passed-in template.
-  String doSanitizeHtml(String template, { bool withOptionalParameters = true }) {
-    if (!withOptionalParameters) {
+  //  * When `withOptionalConfiguration` is `true`: `allowElementId`, `allowClassName`
+  // and `addLinkRel` overrides are passed to the sanitizeHtml call of `template`.
+  // (This is the default behavior for the [testContains]/[testNotContains] methods.)
+  //  * When `withOptionalConfiguration` is false: only `template` is passed.
+  String doSanitizeHtml(String template,
+      {@required bool withOptionalConfiguration}) {
+    if (!withOptionalConfiguration) {
       return sanitizeHtml(template);
     }
 
     return sanitizeHtml(
-        template,
-        allowElementId: (id) => id == 'only-allowed-id',
-        allowClassName: (className) => className == 'only-allowed-class',
-        addLinkRel: (href) => href == 'bad-link' ? ['ugc', 'nofollow'] : null,
-      );
+      template,
+      allowElementId: (id) => id == 'only-allowed-id',
+      allowClassName: (className) => className == 'only-allowed-class',
+      addLinkRel: (href) => href == 'bad-link' ? ['ugc', 'nofollow'] : null,
+    );
   }
 
-  void testContains(String template, String needle, { bool withOptionalParameters = true }) {
+  void testContains(String template, String needle,
+      {bool withOptionalConfiguration = true}) {
     test('"$template" does contain "$needle"', () {
       final sanitizedHtml = doSanitizeHtml(
         template,
-        withOptionalParameters: withOptionalParameters,
+        withOptionalConfiguration: withOptionalConfiguration,
       );
       expect(sanitizedHtml, contains(needle));
     });
   }
 
-  void testNotContains(String template, String needle, { bool withOptionalParameters = true }) {
+  void testNotContains(String template, String needle,
+      {bool withOptionalConfiguration = true}) {
     test('"$template" does not contain "$needle"', () {
       final sanitizedHtml = doSanitizeHtml(
         template,
-        withOptionalParameters: withOptionalParameters,
+        withOptionalConfiguration: withOptionalConfiguration,
       );
       expect(sanitizedHtml, isNot(contains(needle)));
     });
@@ -142,9 +145,13 @@ void main() {
 
   group('Optional parameters stay optional:', () {
     // If any of these fail, it probably means a major version bump is required.
-    testContains('<a href="any-href">hey', 'href=', withOptionalParameters: false);
-    testNotContains('<a href="any-href">hey', 'rel=', withOptionalParameters: false);
-    testNotContains('<span id="any-id">hello</span>', 'id=', withOptionalParameters: false);
-    testNotContains('<span class="any-class">hello</span>', 'class=', withOptionalParameters: false);
+    testContains('<a href="any-href">hey', 'href=',
+        withOptionalConfiguration: false);
+    testNotContains('<a href="any-href">hey', 'rel=',
+        withOptionalConfiguration: false);
+    testNotContains('<span id="any-id">hello</span>', 'id=',
+        withOptionalConfiguration: false);
+    testNotContains('<span class="any-class">hello</span>', 'class=',
+        withOptionalConfiguration: false);
   });
 }
