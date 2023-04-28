0e737a12b81252021a7ffcddba499edaf6a3aaff
Jonas Finnemann Jensen <jonasfj@google.com>
2023-04-28 13:21:11 +0200

    Fix lints in neat_cache

--- neat_cache/lib/src/providers/redis.dart
+++ neat_cache/lib/src/providers/redis.dart
@@ -170,6 +170,8 @@ class RedisCacheProvider extends CacheProvider<List<int>> {
 
         // Force close the client
         scheduleMicrotask(() => client.close(force: true));
+
+        return null;
       });
 
   @override
