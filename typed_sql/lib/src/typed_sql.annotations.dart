87f396e661a46dd1e27984fbee9ff7784b8097c1
Jonas Finnemann Jensen <jonasfj@google.com>
2025-12-04 22:30:14 +0100

    Use docImport in package:typed_sql (#316)
    * Use docImport in package:typed_sql
    
    * Improve documentation

--- typed_sql/lib/src/typed_sql.annotations.dart
+++ typed_sql/lib/src/typed_sql.annotations.dart
@@ -156,12 +156,12 @@ final class Unique {
 ///
 /// Subclasses must:
 ///  * specify a concrete `T` that will be the serialized representation type. As one of:
-///    - String
-///    - Uint8List
-///    - bool
-///    - int
-///    - double
-///    - DateTime
+///    - [String],
+///    - [Uint8List],
+///    - [bool],
+///    - [int],
+///    - [double], or,
+///    - [DateTime].
 ///  * have a `fromDatabase(T value)` constructor.
 ///
 /// If a subclass implements [Comparable] then the encoded values returned by
@@ -170,7 +170,7 @@ final class Unique {
 /// > [!TIP]
 /// > Custom types are not required to be _immutable_ but using an immutable
 /// > type is recommended. The analyzer can help with you this if you use the
-/// > `@immutable` annotation from [`package:meta`][1].
+/// > [immutable] annotation from [`package:meta`][1].
 ///
 /// [1]: https://pub.dev/packages/meta
 ///
