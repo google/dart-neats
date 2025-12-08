f9bed03bb166096adeb7092e007253e5175ae341
Jonas Finnemann Jensen <jonasfj@google.com>
2025-12-08 14:52:29 +0100

    Unique tests (#321)
    * Support JsonValue in DefaultValue annotations
    
    * Updated documentation
    
    * Run dart fix on generated code
    
    This fixes double quotes to single quote when tests contains JSON.
    
    * Fix analysis issues
    
    * Add tool/build.sh
    
    * Test for UNIQUE with different types
    
    * Remove license headers from generated files

---
+++ typed_sql/test/typed_sql/unique/types/unique_text/unique_text_test.dart
@@ -0,0 +1,66 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+//
+// See tool/generate_unique_tests.dart
+
+import 'package:typed_sql/typed_sql.dart';
+
+import '../../../testrunner.dart';
+
+part 'unique_text_test.g.dart';
+
+final _value1 = 'hello world';
+final _value2 = 'Hej verden';
+
+abstract final class PrimaryDatabase extends Schema {
+  Table<Item> get items;
+}
+
+@PrimaryKey(['id'])
+abstract final class Item extends Row {
+  @AutoIncrement()
+  int get id;
+
+  @Unique.field()
+  @SqlOverride(dialect: 'mysql', columnType: 'VARCHAR(255)')
+  String get value;
+}
+
+void main() {
+  final r = TestRunner<PrimaryDatabase>(
+    setup: (db) async {
+      await db.createTables();
+    },
+  );
+
+  r.addTest('db.items.insert', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+    await db.items.insert(value: toExpr(_value2)).execute();
+  });
+
+  r.addTest('db.items.insert (unique violation)', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+    try {
+      await db.items.insert(value: toExpr(_value1)).execute();
+      fail('expected violation of unique constraint!');
+    } on OperationException {
+      return;
+    }
+  });
+
+  r.addTest('db.items.byValue()', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+    await db.items.insert(value: toExpr(_value2)).execute();
+
+    final item = await db.items.byValue(_value1).fetch();
+    check(item).isNotNull().value.equals(_value1);
+  });
+
+  r.addTest('db.items.byValue() - not found', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+
+    final item = await db.items.byValue(_value2).fetch();
+    check(item).isNull();
+  });
+
+  r.run();
+}
---
+++ typed_sql/test/typed_sql/unique/types/unique_zero_real/unique_zero_real_test.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+//
+// See tool/generate_unique_tests.dart
+
+import 'package:typed_sql/typed_sql.dart';
+
+import '../../../testrunner.dart';
+
+part 'unique_zero_real_test.g.dart';
+
+final _value1 = 0.0;
+final _value2 = 3.14;
+
+abstract final class PrimaryDatabase extends Schema {
+  Table<Item> get items;
+}
+
+@PrimaryKey(['id'])
+abstract final class Item extends Row {
+  @AutoIncrement()
+  int get id;
+
+  @Unique.field()
+  double get value;
+}
+
+void main() {
+  final r = TestRunner<PrimaryDatabase>(
+    setup: (db) async {
+      await db.createTables();
+    },
+  );
+
+  r.addTest('db.items.insert', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+    await db.items.insert(value: toExpr(_value2)).execute();
+  });
+
+  r.addTest('db.items.insert (unique violation)', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+    try {
+      await db.items.insert(value: toExpr(_value1)).execute();
+      fail('expected violation of unique constraint!');
+    } on OperationException {
+      return;
+    }
+  });
+
+  r.addTest('db.items.byValue()', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+    await db.items.insert(value: toExpr(_value2)).execute();
+
+    final item = await db.items.byValue(_value1).fetch();
+    check(item).isNotNull().value.equals(_value1);
+  });
+
+  r.addTest('db.items.byValue() - not found', (db) async {
+    await db.items.insert(value: toExpr(_value1)).execute();
+
+    final item = await db.items.byValue(_value2).fetch();
+    check(item).isNull();
+  });
+
+  r.run();
+}
